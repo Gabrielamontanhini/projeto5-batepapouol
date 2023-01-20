817e567bb4098cb7bddb0b5b4944676909a452e9
Gabriela Montanhini de Oliveira <gabrielamontanhini@gmail.com>
2023-01-20 20:10:28 -0300

    conseguindo ver a array de msgs, nada mais

--- script.js
+++ script.js
@@ -1,49 +1,75 @@
+let nome = prompt("Qual é o seu nome?");
+let mensagensDoChat = [];
+
+
+
+function mostrarMensagensDoChat(){
+    const chat = document.querySelector('ul');
+    chat.innerHTML= '';
+    for (let i=0; i<mensagensDoChat.length; i++){
+        let template = `<li>${mensagensDoChat[i].name} ${mensagensDoChat[i].text} </li>`
+    }
+}
 function entrarNoChat(){
 
-
-const nomeDoUsuario = prompt("Qual é o seu nome?")
-
-let login = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeDoUsuario)
-console.log(login)
-
-}
-
-
-
-let mensagens = []
-
-function exibirMensagen(){
-    let Conversa = document.querySelector("ul");
-    Conversa.innerHTML = ''
-
-    for (let i=0; i<mensagens.length; i++){
-        let template = `
-        <li class="todos"> ${mensagens[i].de} para Todos: ${mensagens[i].texto}</li>
-        `
-        Conversa.innerHTML = Conversa.innerHTML + template;
+    let nomeDoUsuario= {
+        name: nome,
     }
+    let enviarnome = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", nomeDoUsuario);
+    console.log("Foi enviado ao servidor");
+    enviarnome.then(nomeRecebido);
+    enviarnome.catch(nomeNotRecebido)
 }
 
 
+function nomeRecebido(response){
+    console.log("O servidor recebeu o nome")
+}
+
+function nomeNotRecebido(response){
+    console.log("O servidor não recebeu o nome")
+}
+
+function manterConexao(){
+    let conexao = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nomeDoUsuario)
+}
 
 function adicionarMensagem(){
-    let mensagemDoUsuario = document.querySelector("input").value;
+    let mensagemDigitada = document.querySelector('input').value;
+let novaMensagem = {
+        from: nome,
+        to: "Todos",
+        text: mensagemDigitada,
+        type: "message",
+}
 
-    const novaMensagem = {
-        de: nomeDoUsuario,
-        texto: mensagemDoUsuario,
-    }
-
-let promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', novaMensagem);
-console.log('enviada');
-promessa.then(RespostaChegou);
-
-    exibirMensagen()
-    document.querySelector("input").value = ''
-
+    let mandandomsg = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", novaMensagem);
+    mandandomsg.then(mensagemMandada);
+    mandandomsg.catch(mensagemNaoFoi);
 
 }
 
+function mensagemMandada(response){
+    console.log("Servidor recebeu a mensagem")
 
+    exibirMensagem()
+}
 
-/* o que precisa fazer : a promessa é o axios post com o link e o que deve ser postado no servido*/
+function mensagemNaoFoi(responde){
+    console.log("Deu algum erro")
+}
+
+function exibirMensagem(){
+    let mostrarAsMsgs = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+    mostrarAsMsgs.then(mostraElas);
+    mostrarAsMsgs.catch(naoVaiMostrar);
+}
+
+function mostraElas(response){
+    console.log(response.data);
+    let mensagensDoChat = response.data;
+}
+
+function naoVaiMostrar(response){
+    console.log("Deu erro")
+}
